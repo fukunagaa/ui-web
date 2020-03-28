06c7afb29575e7c0a061c47c9780c922821eb91d
fukunagaa <k.k.0627ku@gmail.com>
2020-03-28 10:03:38 +0900

    配列初期化

--- src/app.js
+++ src/app.js
@@ -29,19 +29,20 @@ search.addEventListener('click', () => {
     periodFrom = inputYearFrom * 10 + monthFrom;
 
     // 取引時期To 整形
-
     let monthTo = exsample(inputMonthTo);
     periodTo = inputYearTo * 10 + monthTo;
 
-    // 入力エリアコード
+    // 入力エリア
     let areaCode = document.getElementById("input-area-code").value;
+    let areaName = document.getElementById("input-area-name").value;
+    let sample;
     /*
      * 不動産取引価格情報取得API
     */
-    let sample;
-
     transactionPeriod(areaCode)
         .then(response => {
+            transactionPeriodList = [];
+            while (ulArea1.firstChild) ulArea1.removeChild(ulArea1.firstChild);
             for (let i of Object.keys(response.data)) {
                 const transactionPeriodConstructor = new TransactionPeriodConstructor(response.data[i].id, response.data[i].name);
                 transactionPeriodList.push(transactionPeriodConstructor);
@@ -56,20 +57,25 @@ search.addEventListener('click', () => {
 
 
 
-    /*
-     * 都道府県内市区町村一覧取得
-    */
+
     console.log("from : " + periodFrom);
     console.log("to : " + periodTo);
 
+    /*
+     * 都道府県内市区町村一覧取得
+    */
     estate(periodFrom, periodTo, areaCode)
         .then(response => {
             estateList = [];
+            while (ulArea2.firstChild) ulArea2.removeChild(ulArea2.firstChild);
             for (let i of Object.keys(response.data)) {
                 const estateConstructor = new EstateConstructor(response.data[i].Municipality, response.data[i].DistrictName, response.data[i].TradePrice, response.data[i].Area, response.data[i].BuildingYear);
                 estateList.push(estateConstructor);
                 creatLiElement(ulArea2, estateConstructor.info);
             }
+            // Array.from(document.getElementsByTagName("ul")).forEach (element => {
+            //     element.classList.add("ulAnimate");
+            // });
         })
 }, false);
 
@@ -77,4 +83,5 @@ const creatLiElement = (ulArea, value) => {
     const newLi = document.createElement("li");
     newLi.innerText = value;
     ulArea.appendChild(newLi);
+
 }
